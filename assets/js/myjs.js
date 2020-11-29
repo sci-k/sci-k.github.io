b81f76a3173cd9ea69663d83402e2a88c444d721
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2020-11-29 01:51:39 +0100

    restyled it

---
+++ assets/js/myjs.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function myFunction(this_div) {
+  var dots = document.getElementById(this_div+"_dots");
+  var moreText = document.getElementById(this_div+"_more");
+  var btnText = document.getElementById(this_div+"_myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = 'Read more';
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = 'Read less';
+    moreText.style.display = "inline";
+  }
+}
