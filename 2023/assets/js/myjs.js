451b85ca172e1329ae099bc5293561195819afee
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2023-04-23 12:48:40 +0100

    updated the program of the workshop

--- 2023/assets/js/myjs.js
+++ 2023/assets/js/myjs.js
@@ -24,17 +24,17 @@ function myFunction(this_div) {
 
 function adjust_times_CEST() {
     $(".workshopitem").each(function (index) {
-        $(this).text(moment.tz($(this).data("needed"), "Europe/Paris").format("HH:mm"));
+        $(this).text(moment.tz($(this).data("needed"), "America/Ojinaga").format("HH:mm"));
     });
 
     //$("#time-warning").html('All times are in <b>CEST</b>.');
-    $("#time-warning").html('All times are in <b>CEST</b>. Do you want to see them adjusted to your time zone (' + moment().tz(moment.tz.guess()).format('z') + ')? <a href="javascript:void(0)" onclick="adjust_times_user_time_zone();">Click here.</a>');
+    $("#time-warning").html('All times are in <b>Central Daylight Time (CDT)</b>. Do you want to see them adjusted to your time zone (' + moment().tz(moment.tz.guess()).format('z') + ')? <a href="javascript:void(0)" onclick="adjust_times_user_time_zone();">Click here.</a>');
 }
 
 function adjust_times_user_time_zone() {
     var user_time_zone = moment.tz.guess();
     $(".workshopitem").each(function (index) {
-        var adjusted_time = moment.tz($(this).data("needed"), "Europe/Paris").tz(user_time_zone);
+        var adjusted_time = moment.tz($(this).data("needed"), "America/Ojinaga").tz(user_time_zone);
         var text_displacement = '';
         var day_displacement = moment(adjusted_time.format("YYYY-MM-DD")).diff(moment($(this).data("needed")).format("YYYY-MM-DD"), 'days');
         if (day_displacement < 0) {
@@ -45,8 +45,8 @@ function adjust_times_user_time_zone() {
         $(this).text(adjusted_time.format("HH:mm") + text_displacement);
     });
     var formatted_user_time_zone = moment().tz(user_time_zone).format('z');
-    if (formatted_user_time_zone != "CEST" && formatted_user_time_zone != "CET") {
-        $("#time-warning").html('All times are adjusted to <b>your time zone (' + formatted_user_time_zone + ')</b>. Do you want to see times in CEST? <a href="javascript:void(0)" onclick="adjust_times_CEST();">Click here.</a>');
+    if (formatted_user_time_zone != "CDT") {
+        $("#time-warning").html('All times are adjusted to <b>your time zone (' + formatted_user_time_zone + ')</b>. Do you want to see times in Central Daylight Time (CDT)? <a href="javascript:void(0)" onclick="adjust_times_CEST();">Click here.</a>');
     } else {
         $("#time-warning").html('All times are adjusted to <b>your time zone (' + formatted_user_time_zone + ')</b>.');
     }
